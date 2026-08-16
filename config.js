57638f560fac0d50cdbfce2540c75703e79f8614
emguayasamin <emguayasamin96@gmail.com>
2026-08-16 18:15:16 -0500

    Set WhatsApp contact number

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_CONFIG={url:"https://umbsnalizrvdaxaisjij.supabase.co",publishableKey:"sb_publishable_Sc5laHLLdXVh8BOKoeuuxA_AxOL08Bm",functionUrl:"https://umbsnalizrvdaxaisjij.supabase.co/functions/v1/reserve-numbers"};
-window.RAFFLE_CONFIG={name:"Número de la Suerte",prize:"PlayStation 5",price:1,totalNumbers:1000,drawDate:"",whatsappNumber:""};
+window.RAFFLE_CONFIG={name:"Número de la Suerte",prize:"PlayStation 5",price:1,totalNumbers:1000,drawDate:"",whatsappNumber:"593997944319"};
